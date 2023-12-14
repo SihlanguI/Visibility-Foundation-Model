e8ea87f27e1c75d4643d25c24dee4ad95bbeb40c
tamerakassie <65033022+tamerakassie@users.noreply.github.com>
2023-12-14 16:28:28 +0200

    added block layer and starter for the Decoder layer

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 import math
 import torch
 import torch.nn as nn
@@ -8,15 +8,14 @@ import logging
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import vis_access
-import corrprods
+#import tensorflow as tf
 from torch.nn import functional as F 
 
 #Define Hyperparameters Required
 
-batch_size = 8
-block_size = 4
+
+block_size = 4  # B Essentially the context length, in this case predicts 5th token, will change the paramter later
+batch_size = 8  # T
 max_iters = 3000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_interval = 300
@@ -39,7 +38,7 @@ class LayerNorm(nn.Module):
 
 
 class MaskedAttention(nn.Module):
-    def __init__(self, data_size, head_size):
+    def __init__(self, d_model, head_size):
         super(MaskedAttention, self).__init__()
 
         """
@@ -47,9 +46,9 @@ class MaskedAttention(nn.Module):
         the dimensionality of the Q,K,V input projects.
 
         """
-        self.Q = nn.Linear(data_size, head_size, bias=False)
-        self.K = nn.Linear(data_size, head_size, bias=False)
-        self.V = nn.Linear(data_size, head_size, bias=False)
+        self.Q = nn.Linear(d_model, head_size, bias=False)
+        self.K = nn.Linear(d_model, head_size, bias=False)
+        self.V = nn.Linear(d_model, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(batch_size, batch_size)))  #Shape (T, T)
 
     def forward(self, torch_tensor):
@@ -109,11 +108,11 @@ class PositionalEncoder(nn.Module):
 
         self.register_buffer('pe', pe)
 
-    def forward(self, x:torch_tensor):
-        x = x+ self.pe[:, : x.size(1)].requires_grad_(False) 
+    def forward(self, OutputAttention):
+        OutputAttention = OutputAttention+ self.pe[:, : OutputAttention.size(1)].requires_grad_(False) 
 
 
-        return self.dropout(x)
+        return self.dropout(OutputAttention)
     
 
 
@@ -129,7 +128,7 @@ class PositionwiseFFN(nn.Module):
         self.w2 = nn.Linear(d_ffn, d_model)
         self.dropout = nn.Dropout(dropout)  
 
-    def forward(self, torch_tensor):
+    def forward(self, OutputAttention):
         """
         Parameters:
         ----------
@@ -137,26 +136,17 @@ class PositionwiseFFN(nn.Module):
 
         Returns:
         -------
-
-
         """
-        self.net = self.w2(self.dropout(self.w1(torch_tensor).relu()))
+        self.net = self.w2(self.dropout(self.w1(OutputAttention).relu()))
 
         return self.net
     
-
-
-class ResidualBlock(nn.Module):
-    def __init__():
-        super().__init__()
-
-        """
-        
-        
-        """
-
+torch_tensor = torch.zeros(block_size, batch_size, 8)
 class Block(nn.Module):
     def __init__(self,):
+
+        data_size = len(torch_tensor)
+        d_model = torch_tensor.size(0) #B
         super().__init__()
         """
         The purpose of the block layer is to apply the layer normalization, masked attention, and feedforward network. 
@@ -166,12 +156,28 @@ class Block(nn.Module):
         This layer outputs the addition of the original inputs and outputs of the attention layer.
 
         """
- 
+        self.ln1 = LayerNorm(data_size)
+        head_size = data_size//d_model
+        self.MaskedAttention = MaskedAttention(d_model, head_size)
+        self.ffd = PositionwiseFFN(data_size)
+        self.ln2 = LayerNorm(data_size)
+
+    def forward(self, x ):
+
+        x = x + self.MaskedAttention(self.ln1)
+        x = x + self.ffd(self.ln2)
+        
+        return x
 
 
-class Decoder(nn.Module):
+
+class TransformerDecoder(nn.Module):
     def __init__(self,):
         super().__init__()
+
+        
+        
+    
 
     
 
