5bee50220a0e756ae994e88c023f43f36f390108
richarms <richarms@ska.ac.za>
2024-01-17 09:47:58 +0000

    general refactor of the source directory

--- train.py
+++ train.py
@@ -1,38 +1,36 @@
 import numpy as np
-import math
 import torch
 import torch.nn as nn
 import katdal
-import argparse
-import logging
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import vis_access
-import corrprods
-from torch.nn import functional as F
 
 
-#Define Hyperparameters Required
-
+# Hyperparameters
 batch_size = 4
 block_size = 8 
 max_iters = 3000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_interval = 300
 learning_rate = 1e-2
-device = torch.device('cpu')
 eval_iters = 200
 dropout = 0.1
 
-#DataLoader 
-
-"""
-Connecting directly to the Archive.
-"""
-
-path="https://archive-gw-1.kat.ac.za/1701021676/1701021676_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNzAyNDczOTY2LCJwcmVmaXgiOlsiMTcwMTAyMTY3NiJdLCJleHAiOjE3MDMwNzg3NjYsInN1YiI6InRrYXNzaWVAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.CLCieZa-9IRgV4HrD8O37I9RwxvsavQu_KljILjP2uTUEiB9ePwVUia4Td4RZ_7c7xz-HvikgfCkxpo7LvyCbQ"
+def get_batch(split):
+    data_test =  train_points if split == 'train' else test_points         
+    ix = torch.randint(len(data_test)-block_size, (batch_size, )) 
+    x = torch.stack([data_test[i:i+block_size] for i in ix])
+    y = torch.stack([data_test[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+    
+def get_tensorBTC(xb):
+    xb_BTC = tf.expand_dims(xb, axis=2)
+    xb_BTC_new = tf.tile(xb_BTC , [1, 1, 8]) 
+    numpy_array = xb_BTC_new.numpy()
+    torch_tensor = torch.tensor(numpy_array, dtype=torch.int64)
+    B,T,C = torch_tensor.shape
+    return torch_tensor, B,T,C
 
 def read_rdb(path):
     data = katdal.open(path)
@@ -47,30 +45,15 @@ def read_rdb(path):
     data_size = len(data_test)
     return data_test, data_size
 
-data_test = read_rdb(path)
-data_size = read_rdb(path)
+path="https://archive-gw-1.kat.ac.za/1701021676/1701021676_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNzAyNDczOTY2LCJwcmVmaXgiOlsiMTcwMTAyMTY3NiJdLCJleHAiOjE3MDMwNzg3NjYsInN1YiI6InRrYXNzaWVAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.CLCieZa-9IRgV4HrD8O37I9RwxvsavQu_KljILjP2uTUEiB9ePwVUia4Td4RZ_7c7xz-HvikgfCkxpo7LvyCbQ"
 
+data_test, data_size = read_rdb(path)
 n = int(0.8*len(data_test)) 
 train_points = data_test[:n]
 test_points = data_test[n:]
 
-def get_batch(split):
-
-    data_test =  train_points if split == 'train' else test_points  
-                                                                           
-    ix = torch.randint(len(data_test)-block_size, (batch_size, )) 
-    x = torch.stack([data_test[i:i+block_size] for i in ix])
-    y = torch.stack([data_test[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-
 xb, yb = get_batch('train')
 
-def get_tensorBTC(xb):
-    xb_BTC = tf.expand_dims(xb, axis=2)
-    xb_BTC_new = tf.tile(xb_BTC , [1, 1, 8]) 
-    numpy_array = xb_BTC_new.numpy()
-    torch_tensor = torch.tensor(numpy_array, dtype=torch.int64)
-    B,T,C = torch_tensor.shape
-    return torch_tensor, B,T,C
+
+
+
--- vis_access.py
+++ vis_access.py
@@ -2,7 +2,9 @@ import katdal
 from katdal.lazy_indexer import DaskLazyIndexer
 from katdal.lazy_indexer import LazyTransform
 
+import katdal
 import numpy as np
+import pandas as pd
 
 def read_rdb(path):
     """
@@ -62,3 +64,36 @@ def vis_per_scan(data):
         flag_chunk = np.zeros_like(vis_chunk, dtype=np.bool)
         load(data, np.s_[:, :, :], vis_chunk, weight_chunk, flag_chunk)
     return vis_chunk, flag_chunk
+
+
+def get_corrprods(vis):
+    bl = vis.corr_products
+    bl_idx = []
+    for i in range(len(bl)):
+        bl_idx.append((bl[i][0][0:-1]+bl[i][1][0:-1]))
+    return np.array(bl_idx)
+    
+
+def get_bl_idx(vis, nant):
+    """
+    Get the indices of the correlation products.
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+    nant : int
+       number of antennas
+
+    Returns:
+    --------
+    bl_idx : numpy array of baseline indices
+    """
+    nant = nant
+    A1, A2 = np.triu_indices(nant, 1)
+    # Create baseline antenna combinations
+    corr_products = np.array(['m{:03d}m{:03d}'.format(A1[i], A2[i]) for i in range(len(A1))])
+    df = pd.DataFrame(data=np.arange(len(A1)), index=corr_products).T
+    corr_prods = get_corrprods(vis)
+    bl_idx = df[corr_prods].values[0].astype(np.int32)
+    return bl_idx
