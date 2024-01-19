638115503ce2ccf0bf8449fefea1e97d7944f773
Tamera Kassie <tamerakassie10@gmail.com>
2024-01-19 11:15:25 +0000

    model and training script updated, loss converges, with no nan/inf

--- model.py
+++ model.py
@@ -1,30 +1,26 @@
-
 import math
 import torch
 import torch.nn as nn
-import katdal
-import argparse
-import logging
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
-#import tensorflow as tf
 from torch.nn import functional as F 
+from torch.nn import init
+
+
+torch.manual_seed(1337)
+
 
 #Define Hyperparameters Required
-
-
-block_size = 4  # B Essentially the context length, in this case predicts 5th token, will change the paramter later
-batch_size = 8  # T
-max_iters = 3000
+block_size = 8 # B Essentially the context length, in this case predicts 5th token, will change the paramter later
+batch_size = 256  # T
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_interval = 300
-learning_rate = 1e-2
-device = torch.device('cpu')
-eval_iters = 200
-n_layer = 4 # number of layers for the deep NN
-dropout = 0.1
-#d_ff = 4*d_model #From Paper
+n_layer = 6 # number of layers for the deep NN
+p = 0.1
+d_model = 8
+d_ff = 4*d_model #From Paper
+n_head = 4
+head_size=16
+
 
 class LayerNorm(nn.Module):
     """
@@ -41,12 +37,12 @@ class LayerNorm(nn.Module):
         self.alpha = nn.Parameter(torch.ones(1)) # Multipled to the input #Makes the parameter learnable
         self.bias = nn.Parameter(torch.ones(1))  # Addition parameter to the input
 
-        
+
 
     def forward(self,Input):
 
         """
-        The Layer Norm Forward uses the mean standardising formular to normalise the input batches. 
+        The Layer Norm Forward uses the mean standardising formular to normalise the input batches.
         Option can also be to use F.LayerNorm from pytorch.
         """
 
@@ -55,8 +51,6 @@ class LayerNorm(nn.Module):
         x_nu = self.alpha *(Input-mean)/(std+self.eps)+self.bias
 
         return x_nu
-
-
 class MaskedAttention(nn.Module):
     def __init__(self, d_model, head_size):
         super(MaskedAttention, self).__init__()
@@ -64,47 +58,53 @@ class MaskedAttention(nn.Module):
         """
         Causal/Masked Attention Class:
 
-        Map Q.K,V of input size data_size to head_size, this is done to control 
+        Map Q.K,V of input size data_size to head_size, this is done to control
         the dimensionality of the Q,K,V input projects.
 
         """
-        self.Q = nn.Linear(d_model, head_size, bias=False)
+
+        self.head_size = head_size
+        self.Q = nn.Linear(d_model,head_size, bias=False)
         self.K = nn.Linear(d_model, head_size, bias=False)
         self.V = nn.Linear(d_model, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(batch_size, batch_size)))  #Shape (T, T)
+        #self.register_buffer('tril torch.tril(torch.ones(T, T))) #Shape (T, T)
+        #The above doesnt seem to work well replace with this implemnetaion of the triangular matrix torch.ones
 
-    def forward(self, torch_tensor):
+    def forward(self, xtorch_tensor):
         """
         Apply the linear layers self.K &self.Q to compute the respective key and query tensors.
 
         """
-        B,T,C = torch_tensor
-        key = self.K(torch_tensor.float())    #Shape (B,T,C)
-        query = self.Q(torch_tensor.float())  #Shape (B,T,C)
-        value = self.V(torch_tensor.float())
+        B, T,C = xtorch_tensor.shape
+
+        key = self.K(xtorch_tensor)    #Shape (B,T,C)
+        query = self.Q(xtorch_tensor)  #Shape (B,T,C)
+        value = self.V(xtorch_tensor)
+        trill =  torch.tril(torch.ones((T,T))).to('cuda')
+        #print("query shape:", query.shape)
+        #print("key shape:", key.shape)
 
         """
         Compute the Attention Scores:
         * Dot product of the query tensor with the transpose of the key tensor.
         * Mask applied to the scaled weights.
         * Normalise the scaled weights by applying softmax.
-        * Matrix Multiply the populated weight (average*scaled*normalised) by the value tensor 
+        * Matrix Multiply the populated weight (average*scaled*normalised) by the value tensor
 
-        Returns: Scaled Attention 
-        
+        Returns: Scaled Attention
+
         """
 
-        wei = query @ key.transpose(-2,-1)*C**-0.5         #Shapes (B, T, C) @ (B, C, T) ----> (B, T, T)  #Normalised using scaled attention
-        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) 
+        wei = query @ key.transpose(-2,-1)        #Shapes (B, T, C) @ (B, C, T) ----> (B, T, T)  #Normalised using scaled attention
+        wei = wei.masked_fill(trill == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
-        Output = wei @ value                              #Shape (B, T, headsize)
+        Output = wei @ value
+        #print(Output.shape)                             #Shape (B, T, headsize)  Bug returning (B, T, C*head_size)
 
         return Output
 
-
-
 class PositionalEncoder(nn.Module):
-    def __init__(self, d_model: int, dropout:float=0.1,max_len: int=5000 ):
+    def __init__(self, d_model: int, batch_size : int,  dropout:float=0.1,max_len: int=10 ):
 
         """
         Parameters:
@@ -117,30 +117,37 @@ class PositionalEncoder(nn.Module):
 
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-     
-        pe = torch.zeros(max_len, d_model)    
-        k = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
-   
-        pe[:, 0::2] = torch.sin(k * div_term)    
-  
-        pe[:, 1::2] = torch.cos(k * div_term)  
-    
-        pe = pe.unsqueeze(0) 
+        self.max_len = max_len
+        self.d_model = d_model
+        self.batch_size = batch_size
+
+        pe = torch.zeros(max_len, d_model)
+        #print("Shape pe:",np.shape(pe))
+        k = torch.arange(0,max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(k * div_term)
+
+        pe[:, 1::2] = torch.cos(k * div_term)
+
+        pe = pe.unsqueeze(0) #tensor (1,max_len,  batch_size, d_model)
 
         self.register_buffer('pe', pe) #The position will be saved in a register (not be updated with the model)
 
-    def forward(self, x):  
-        x = x+ self.pe[:, :x.size(1)].requires_grad_(False) # Adds the X inputtesnor to the positions, the requires_grad makes sure that the positions are not learnt (ie the positions will not be updated with weights/bias)
-        Output = self.dropout(x)
+    def forward(self, xtorch_tensor):
+
+        #print("Shape xtensor:",np.shape(xtorch_tensor))
+
+        xtorch_tensor = xtorch_tensor + self.pe[:,:xtorch_tensor.size(1),:].to('cuda').requires_grad_(False)  # Adds the X inputtesnor to the positions, the requires_grad makes sure that the positions are not learnt (ie the positions will not be updated with weights/bias)
+        Output = self.dropout(xtorch_tensor)
 
 
-        return Output
+        return Output.to('cuda')
     
 
 
 class PositionwiseFFN(nn.Module):
-    def __init__(self, d_model: int, d_ffn: int, dropout: float=0.1):
+    def __init__(self, d_model: int, dropout):
         super().__init__()
         """
         FeedForward Class:
@@ -150,11 +157,11 @@ class PositionwiseFFN(nn.Module):
 
         """
 
-        self.L1 = nn.Linear(d_model, d_ffn)      
-        self.L2 = nn.Linear(d_ffn, d_model)
-        self.dropout = nn.Dropout(dropout)  
+        self.L1 = nn.Linear(d_model, d_model)      
+        self.L2 = nn.Linear( d_model, d_model)
+        self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, Output):
+    def forward(self, input_tensor):
         """
         Parameters:
         ----------
@@ -165,18 +172,18 @@ class PositionwiseFFN(nn.Module):
         -------
         The forward method returns the conversion of the batched inputs.
         """
-        self.FFnet = self.L1(self.dropout(torch.relu(self.L2(Output))))
+        #print("ffwd",np.shape(self.L1))
+        #print("L1 weight shape:", np.shape(self.L1.weight))
+        self.FFnet = self.dropout(torch.relu(self.L2(self.L1(input_tensor))))
 
         return self.FFnet
     
 
 
-torch_tensor = torch.zeros(block_size, batch_size, 8)
-class Block(nn.Module):
-    def __init__(self, d_model):
 
-        data_size = len(torch_tensor)
-        d_model = torch_tensor.size(0) # shape of B
+class Block(nn.Module):
+    def __init__(self, d_model, n_head):
+
         super().__init__()
         """
         The purpose of the block layer is to apply the layer normalization, masked attention, and feedforward network. 
@@ -189,70 +196,115 @@ class Block(nn.Module):
 
         """
         self.ln1 = LayerNorm(d_model)
-        head_size = data_size//d_model
-        self.MaskedAttention = MaskedAttention(d_model, head_size)
-        self.ffd = PositionwiseFFN(d_model)
+        head_size = d_model//n_head
+        #print(f"headsize cal is {head_size}")
+
+        self.MaskedAttention = MaskedAttention(d_model, 8)
+        self.ffd = PositionwiseFFN(d_model, p)
         self.ln2 = LayerNorm(d_model)
 
     def forward(self, x ):
+        #print(x.shape)
+        #print(self.MaskedAttention(self.ln1(x)).shape)
 
-        x = x + self.MaskedAttention(self.ln1)
-        x = x + self.ffd(self.ln2)
-        
+        attention_output = self.MaskedAttention(self.ln1(x))
+        #print("Attention output shape:", attention_output.shape)
+
+        x_attention = x + attention_output
+        #print("Output shape:", x_attention.shape)
+        x_feedforward = self.ffd(self.ln2(x_attention))
+        #print("Feedforward output shape:", x_feedforward.shape) shape is (4, 8,8 )
+        x = x_attention + x_feedforward
+
+
         return x
+
+class ResidualConnection(nn.Module):
+    def __init__(self, p):
+        super().__init__
+        self.dropout = nn.Dropout(p)
+        self.norm = LayerNorm()
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class lmHead(nn.Module):
+    def __init__(self, d_model):
+        super(lmHead, self).__init__()
+        self.linear = nn.Linear(d_model, d_model)
+
+    def forward(self, hidden_states): #Hidden states, is the xtensor that has gone through the above transformer layers
+        logits = self.linear(hidden_states)
+        return logits
 
 
 
 class TransformerDecoder(nn.Module):
-    def __init__(self, d_model, n_head, n_layer):
+    def __init__(self, d_model, n_layer):
         super().__init__()
 
-        d_model = torch_tensor.size(0) # shape of B
-
+        
         """
         No embeddings.
         * Initialise position, block, prediction instances. 
         * Position instance of the PositionalEncoder class will add positional information to the input tensor.
         * Block - Sequence of transformer blocks, the number of blocks are determined by n_layer. 
         * Prediction instance is responsible for predicting the next value.Contains a linear layer to produce final model prediction.
-        """
-        self.position = PositionalEncoder(d_model)
-        self.block = nn.Sequential(*[Block() for _ in range(n_layer)])
-        self.prediction = nn.Linear(d_model,1 ) #set to one, since we predicting the next value
+        """                 
+        self.position = PositionalEncoder(d_model, batch_size)
+        self.dropout = nn.Dropout(p)
+        self.block = nn.Sequential(*[Block(d_model, n_head) for _ in range(n_layer)])
+        self.lmHead = lmHead(d_model)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
 
-    def forward(self, torch_tensor, targets=None):
-
+    def forward(self, xtensor, targets=None):
         """
         The input tensor has been passed through the hidden states of the transfomer blocK
         * x is the tensor that was passed through the block, the prediction instance will project the learnt representations to the oupu dimension.
 
         """
-
-        PositionToken = self.position(torch_tensor)
-        x = torch_tensor+PositionToken
-        x=self.blocks(x)
-        output = self.prediction(x)
-
-
-
-
-
-
-
-        
-        
+        position = self.position(xtensor)
+        x = xtensor+position
+        x = self.dropout(x)
+        x =self.block(x)
+        logits = self.lmHead(x)
+        if targets is not None:
     
+           # logits = logits.view(-1,logits.size(-1))
+           # maxlogits = torch.max(logits, dim=1, keepdim=True).values
+           # normlogits = logits-maxlogits
+           # prob =F.softmax(normlogits, dim=1)
+            #print(f"logits", normlogits.shape)
+           # targets = targets.view(-1, targets.size(-1))
+            #print(f"targets", targets.shape)
+            #print("Logits:", normlogits)
+            #print("Targets:", targets)
+            B,T,C =logits.shape
+            assert logits.shape[0] == targets.shape[0], "Number of elements in targets does not match logits"
+             # Reshape logits to [batch_size * block_size, num_channels]
+            logits_flat = logits.view(B * T, C)
 
-    
+# Reshape targets to [batch_size * block_size]
+            targets_flat = targets.view(-1)
+ 
+           # print("logits shape:", logits.shape)
+           # print("targets shape:", targets.shape)
+            loss =F.mse_loss(logits, targets)
 
 
-
-
-
-
-
-
-
-
-
+            #loss = F.cross_entropy(normlogits, targets, ignore_index=-1)
+        else:
+            logits = self.lmHead(x)
+            loss=None
+        return  logits, loss
+            
--- train.py
+++ train.py
@@ -1,30 +1,36 @@
+
+
+#Training Loop
+import os
+print(os.getcwd())
 import numpy as np
 import math
 import torch
 import torch.nn as nn
 import katdal
-import argparse
-import logging
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import vis_access
-import corrprods
-from torch.nn import functional as F
+from corrprods import get_bl_idx, get_corrprods
+from model import TransformerDecoder
 
 
 #Define Hyperparameters Required
 
-batch_size = 4
-block_size = 8 
-max_iters = 3000
+batch_size = 256
+block_size = 8
+max_iters = 5000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_interval = 300
-learning_rate = 1e-2
-device = torch.device('cpu')
+eval_interval = 100
+learning_rate = 1e-3
 eval_iters = 200
 dropout = 0.1
+max_iters = 5000
+n_layer = 6 # number of layers for the deep NN
+p = 0.1
+d_model = 8
+d_ff = 4*d_model #From Paper
+n_head = 4
+head_size =8
+C=8
+
 
 #DataLoader 
 
@@ -32,31 +38,42 @@ dropout = 0.1
 Connecting directly to the Archive.
 """
 
-path="https://archive-gw-1.kat.ac.za/1701021676/1701021676_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNzAyNDczOTY2LCJwcmVmaXgiOlsiMTcwMTAyMTY3NiJdLCJleHAiOjE3MDMwNzg3NjYsInN1YiI6InRrYXNzaWVAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.CLCieZa-9IRgV4HrD8O37I9RwxvsavQu_KljILjP2uTUEiB9ePwVUia4Td4RZ_7c7xz-HvikgfCkxpo7LvyCbQ"
+path="https://archive-gw-1.kat.ac.za/1701021676/1701021676_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNzA1NDAwMTc3LCJwcmVmaXgiOlsiMTcwMTAyMTY3NiJdLCJleHAiOjE3MDYwMDQ5NzcsInN1YiI6InRrYXNzaWVAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.5qFpWTvtMupQj1w_up5EmAvp3VHE7QUOD2CKkhmsUULerzB5vqVAvDAHgbIwPp6HwZU9qmvuAiJEBmfXuHsqWw"
 
 def read_rdb(path):
     data = katdal.open(path)
-    data.select(dumps = slice(0,10), scans ='track', pol='HH', corrprods='cross')
+    data.select(dumps = slice(0,50), scans ='track', pol='HH', corrprods='cross')
     data_HH = np.zeros((4096, 2016))
-    bl_idx_HH = corrprods.get_bl_idx( data, 64)
-    data_HH[:,:] = np.nan
-    data_HH[:, bl_idx_HH] = data.vis[2,:,:]
+    bl_idx_HH = get_bl_idx( data, 64)
+    data_HH[:,:] = np.zeros_like(data_HH)
+   
+    data_HH[:, bl_idx_HH] = np.abs(data.vis[2,:,:])
     bl_av_HH = np.mean((np.abs(data_HH[:, 0:2016])), axis =0)
     bl_av_HH_np =  np.reshape(bl_av_HH, -1)
-    data_test =  torch.tensor(np.abs(bl_av_HH_np), dtype=torch.long)
+    data_test =  torch.tensor(bl_av_HH_np, dtype=torch.int64)
     data_size = len(data_test)
     return data_test, data_size
 
-data_test = read_rdb(path)
-data_size = read_rdb(path)
+data_test, data_size = read_rdb(path)
 
-n = int(0.8*len(data_test)) 
-train_points = data_test[:n]
-test_points = data_test[n:]
+characters = sorted(list(set(data_test))) #sorts the data corpus
+#print(characters)
+print(len(characters))
+
+character_masked = [char if char>=0 else 0 for char in characters]
+print(character_masked)
+
+
+data = torch.tensor(character_masked, dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+print(data)
+print(data.dtype)
 
 def get_batch(split):
 
-    data_test =  train_points if split == 'train' else test_points  
+    data =  train_points if split == 'train' else test_points  
                                                                            
     ix = torch.randint(len(data_test)-block_size, (batch_size, )) 
     x = torch.stack([data_test[i:i+block_size] for i in ix])
@@ -64,13 +81,64 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
+def get_tensorBTC(xb):
+    xb_BTC = np.tile(xb.cpu()[:,:,np.newaxis], (1,1,8))
+    torch_tensor = torch.tensor(xb_BTC, dtype=torch.int64).to('cuda')
+    B,T,C = torch_tensor.shape
+    #prprint("Tensor Shape:",xtorch_tensor.shape)
+    return torch_tensor, B,T,C  
 
+
+#Loss Estimate for Training
+@torch.no_grad()
+def estimate_loss():
+    #out = {'train':0.0, 'val':0.0
+    out ={}
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            xtorch_tensor, B, T,C = get_tensorBTC(X)
+            ytorch_tensor, B, T,C = get_tensorBTC(Y)
+
+            prob, loss = model(xtorch_tensor, ytorch_tensor.float())
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+   
+
+data_test, data_size = read_rdb(path)
+#print(data_test.dtype)
+n = int(0.8*len(data_test))
+train_points = data_test[:n]
+test_points = data_test[n:]
 xb, yb = get_batch('train')
 
-def get_tensorBTC(xb):
-    xb_BTC = tf.expand_dims(xb, axis=2)
-    xb_BTC_new = tf.tile(xb_BTC , [1, 1, 8]) 
-    numpy_array = xb_BTC_new.numpy()
-    torch_tensor = torch.tensor(numpy_array, dtype=torch.int64)
-    B,T,C = torch_tensor.shape
-    return torch_tensor, B,T,C
+# Training Loop
+model = TransformerDecoder(block_size, n_layer).to('cuda')
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+model.train()  # the model is in training mode
+
+
+for iter in range(max_iters):
+    X, Y = get_batch('train')
+    xtorch_tensor, B, T, C = get_tensorBTC(X)
+    ytorch_tensor, B, T, C = get_tensorBTC(Y)
+    prob, loss = model(xtorch_tensor, ytorch_tensor.float())
+
+    loss.backward()
+  #  clip_grad_norm_(model.parameters(), max_norm=1.0)
+    #for param in model.parameters():
+        #print(param.grad)
+
+    optimizer.step()
+
+    optimizer.zero_grad() # Back propation of the model weights
+
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}")
+    
